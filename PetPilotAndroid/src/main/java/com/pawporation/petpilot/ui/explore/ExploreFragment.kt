ebeb84ea94e4abcef0d2b56cca393a42282492fa
Tina Ho <ho.tina1704@gmail.com>
2023-03-17 17:03:37 -0700

    select/deselect button works

--- PetPilotAndroid/src/main/java/com/pawporation/petpilot/ui/explore/ExploreFragment.kt
+++ PetPilotAndroid/src/main/java/com/pawporation/petpilot/ui/explore/ExploreFragment.kt
@@ -2,10 +2,13 @@ package com.pawporation.petpilot.ui.explore
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.model.Marker
+import com.google.android.material.textview.MaterialTextView
+import com.pawporation.petpilot.android.R
 import com.pawporation.petpilot.android.databinding.FragmentExploreBinding
 
 open class ExploreFragment : Fragment() {
@@ -26,21 +29,7 @@ open class ExploreFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-
-        _binding = FragmentExploreBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-//        searchBar.inflateMenu(R.menu.searchbar_menu)
-//        searchBar.setOnMenuItemClickListener { menuItem -> true }
-
-
-//        val mapFragment = MapFragment()
-//        childFragmentManager.beginTransaction().apply {
-//            add(R.id.map, mapFragment)
-//            commit()
-//        }
-        return root
+        return inflater.inflate(R.layout.fragment_explore, container, false)
     }
 
     @SuppressLint("RestrictedApi")
@@ -50,10 +39,31 @@ open class ExploreFragment : Fragment() {
         supportActionBar?.setShowHideAnimationEnabled(false)
         supportActionBar?.hide()
 
+        val textView = view.findViewById<MaterialTextView>(R.id.explore_selection)
+        textView?.setOnClickListener(selection)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private var selection: View.OnClickListener = View.OnClickListener { view ->
+        val selectAll = resources.getString(R.string.select_all)
+        val deselectAll = resources.getString(R.string.deselect_all)
+        val textView = view as MaterialTextView
+        val currText = textView.text
+
+        placesList.forEach {
+            it!!.isVisible = currText == selectAll
+        }
+
+        textView.text = when (currText) {
+            selectAll -> deselectAll
+            deselectAll -> selectAll
+            else -> {
+                ""
+            }
+        }
     }
 }
